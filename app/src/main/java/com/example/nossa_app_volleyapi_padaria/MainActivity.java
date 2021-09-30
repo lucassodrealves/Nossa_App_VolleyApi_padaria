b7b3a380d29410e99b2121177995691b6d28a04a
lucassodrealves <lucassodrealves4@gmail.com>
2021-09-29 23:03:44 -0300

    Initial commit

---
+++ app/src/main/java/com/example/nossa_app_volleyapi_padaria/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.nossa_app_volleyapi_padaria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
